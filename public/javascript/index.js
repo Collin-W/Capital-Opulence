c5e8d88a6649ef46dea88ad9a1bc2107a1a96d38
Collin-W <whalencollin@gmail.com>
2021-11-26 11:07:30 -0600

    html sections hide and show with jQuery, and buttons added for a dynamic page

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,10 +1,46 @@
 let expenseArray = [];
 
+//$('').hide();
+//$('').show();
+
+//default html elements
+//can this be a var so it is more dry
+$('#expense-form-fieldset').hide();
+$('#login-signup').hide();
+$('#calculator').hide();
+
+//renders or hides html on button clicks
+$('#start-login-signup').click(() => {
+    $('#homepage').hide();
+    $('#login-signup').show();
+});
+
+$('#start-expense-tracker').click(() => {
+    $('homepage').hide();
+    $('#expense-form-fieldset').show();
+});
+
+$('#start-calculator').click(() => {
+    $('#homepage').hide();
+    $('#calculator').show();
+});
+
+$('.go-back-home').click(() => {
+    $('#homepage').show();
+    $('#expense-form-fieldset').hide();
+    $('#login-signup').hide();
+    $('#calculator').hide();
+
+    // add each new section to this 
+})
+
+
 $("#expense-form-add-btn").click(() => {
     console.log('add button')
 
     evt.preventDefault();
 
+    //this will render the html for a new row of form elements on an add button click
     let inputDate = $("<input>", {class: "expense-input expense-date", type: "date", placeholder:"Date Of Expense"});
     let inputGainLoss = $("<input>", {class: "expense-input expense-gain-loss", type: "boolean", placeholder:"Gain/Loss",  list:"gain-loss"});
     let inputDescription = $("<input>", {class: "expense-input expense-description", type: "text", placeholder:"Description Of Expense"});
@@ -16,7 +52,7 @@ $("#expense-form-add-btn").click(() => {
      $('#expense-form').append(newInputRow)
 })
 
-// collect expense form data
+// collect expense form data and pushes it into an array.
 $(".expense-input").each(function () {
     let userInputs = $(this).val().trim();
     expenseArray.push(userInputs);
