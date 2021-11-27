5cdee1a5a0412920c1a885c3ecdf43342198bcad
Mark <mjbc53@gmail.com>
2021-11-27 12:13:24 -0600

    finished setting up the server.js file

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const path = require('path');
-const { dirname } = require('path');
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+
+const sess = {
+    secret: 'Super Secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.use(session(sess))
+//use routes
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
