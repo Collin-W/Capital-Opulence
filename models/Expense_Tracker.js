898d8d42678d364e3558094f088956ea23c27244
Mark <mjbc53@gmail.com>
2021-11-27 12:03:20 -0600

    expense tracker model completed

--- models/Expense_Tracker.js
+++ models/Expense_Tracker.js
@@ -6,7 +6,37 @@ class Expense_Tracker extends Model {}
 
 Expense_Tracker.init(
   {
-
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM('exspense', 'income', ''),
+      allowNull: false,
+      defaultValue: '',
+    },
+    description: {
+      type: DataTypes.STRING(256),
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(7,2),
+      allowNull: false,
+      defaultValue: 0.00
+    },
+    from_user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -15,3 +45,5 @@ Expense_Tracker.init(
     modelName: 'expense_tracker'
   }
 )
+
+module.exports = Expense_Tracker
