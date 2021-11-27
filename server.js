902f14d7eb40866ef61e15d4b5aad3ec7a13d47b
Mark <mjbc53@gmail.com>
2021-11-27 12:29:21 -0600

    fixed bug in server.js file, started on endpoints for user model. layed out route index files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars')
