fb9775c71da30868780f1c74d933fa57c05dd3a4
Mark <mjbc53@gmail.com>
2021-12-01 17:39:25 -0600

    added info in about page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,9 +23,10 @@ router.get('/calc', (req, res) => {
   res.render('calculator')
 })
 
+
 router.get('/about', (req, res) => {
   cardList = [];
-  cardList.push({ imgsrc: "images/blank.jpg", contributor: "Mark Carriveau", title: "Some Title", abouttext: "about mark", github: "mark github", email: "mark@mark.mark" });
+  cardList.push({ imgsrc: "images/blank.jpg", contributor: "Mark Carriveau", title: "Jr. Developer", abouttext: "Aspiring Junior Developer", github: "https://github.com/mjbc53", email: "mjbc53@gmail.com"});
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Tom Cole", title: "Some Title", abouttext: "about tom", github: "tom github", email: "tom@tom.tom" });
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Inmar Luna", title: "Some Title", abouttext: "about inmar", github: "inmar github", email: "inmar@inmar.inmar" });
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Ben Vue", title: "Some Title", abouttext: "about ben", github: "ben github", email: "ben@ben.ben" });
