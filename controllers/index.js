aaf417840686d7ac8662fb0f133a5bfdc9a4cf51
Collin-W <whalencollin@gmail.com>
2021-11-30 22:30:32 -0600

    nothing

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 const apiRoutes = require('./api')
 const homeRoutes = require('./home-routes');
-<<<<<<< HEAD
-
-=======
->>>>>>> 41e0c74c1bd0da1d1eb7e5be96a685060c29cf8b
 const dashboardRoutes = require('./dashboard-routes')
 
 router.use('/api', apiRoutes)
