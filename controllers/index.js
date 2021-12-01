dc8f8e204ba2ce54608105dd6a856637f61eca00
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-11-30 18:59:56 -0600

    pre-worked handlebars/html/css

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
-
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api')
 
 router.use('/api', apiRoutes)
+router.use('/', homeRoutes);
 
 module.exports = router
