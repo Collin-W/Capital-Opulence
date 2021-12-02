91cd3167b6af998802c8e4519d18071e458f8d4f
Caeldeth <sabrael@hybrasyl.com>
2021-12-01 20:26:54 -0600

    about page styling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,9 +27,9 @@ router.get('/calc', (req, res) => {
 router.get('/about', (req, res) => {
   cardList = [];
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Mark Carriveau", title: "Jr. Developer", abouttext: "Aspiring Junior Developer", github: "https://github.com/mjbc53", email: "mjbc53@gmail.com"});
-  cardList.push({ imgsrc: "images/blank.jpg", contributor: "Tom Cole", title: "Some Title", abouttext: "about tom", github: "tom github", email: "tom@tom.tom" });
+  cardList.push({ imgsrc: "images/blank.jpg", contributor: "Tom Cole", title: "Project Manager & Developer", abouttext: "about tom", github: "https://github.com/caeldeth", email: "thomas.cole@eris.co" });
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Inmar Luna", title: "Some Title", abouttext: "about inmar", github: "inmar github", email: "inmar@inmar.inmar" });
-  cardList.push({ imgsrc: "images/ben.jpg", contributor: "Ben Vue", title: "Full Stack Student", abouttext: "Ben is Ben", github: "benyvue", email: "bvue012@gmail.com" });
+  cardList.push({ imgsrc: "images/ben.jpg", contributor: "Ben Vue", title: "Full Stack Student", abouttext: "Ben is Ben", github: "https://github.com/benyvue", email: "bvue012@gmail.com" });
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Collin Whalen", title: "Some Title", abouttext: "about collin", github: "collin github", email: "collin@collin.collin" });
   res.render('about', {cardList: cardList });
 })
