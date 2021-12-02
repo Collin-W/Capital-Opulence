a8c55ba2021ba91b80341bb82cb8219c0bdf1cc4
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-12-01 21:21:17 -0600

    image added/outlined fixing

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-// app.use(express.static('public/images'));
+app.use(express.static('images'));
 
 app.use(session(sess))
 
