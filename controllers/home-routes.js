2d99fb0a10421ee730ced7af2ccb6f9fcb47279d
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2021-12-04 15:49:33 -0600

    media queries for calculator

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,7 +36,7 @@ router.get('/about', (req, res) => {
   cardList = [];
   cardList.push({ imgsrc: "images/mark.jpg", contributor: "Mark Carriveau", title: "Future Jr. Developer", abouttext: "Currently a machinist with a love for coding", github: "https://github.com/mjbc53", email: "mjbc53@gmail.com"});
   cardList.push({ imgsrc: "images/blank.jpg", contributor: "Tom Cole", title: "Project Manager & Developer", abouttext: "about tom", github: "https://github.com/caeldeth", email: "thomas.cole@eris.co" });
-  cardList.push({ imgsrc: "images/blank.jpg", contributor: "Inmar Luna", title: "Some Title", abouttext: "about inmar", github: "inmar github", email: "inmar@inmar.inmar" });
+  cardList.push({ imgsrc: "images/blank.jpg", contributor: "Inmar Luna", title: "Full Stack Student", abouttext: "IT Specialist wanting to be a back-end developer.", github: "https://github.com/LunaZ13", email: "inmar@inmar.inmar" });
   cardList.push({ imgsrc: "images/ben.jpg", contributor: "Ben Vue", title: "Full Stack Student", abouttext: "Ben is Ben", github: "https://github.com/benyvue", email: "bvue012@gmail.com" });
   cardList.push({ imgsrc: "images/collin.jpg", contributor: "Collin Whalen", title: "Front-end Development Enthusiast", abouttext: "I have a passion to create applications that help others!", github: "https://github.com/Collin-W", email: "whalencollin@gmail.com" });
   //render about page
