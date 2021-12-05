1e5af6c54747e2c9ecffeb7a0d5f365a0c0962c9
Collin-W <whalencollin@gmail.com>
2021-12-05 09:55:46 -0600

    refactor and commented expense form.js and css

--- public/javascript/expense-form.js
+++ public/javascript/expense-form.js
@@ -1,26 +1,22 @@
 let expenseArray = [];
+
+//two special counters, the del counter is specifically for the delete button on each rendered rows
 let i = 0;
 let del = 0;
 
 // listens to the expense form only
 $("#expense-form-add-btn").click((evt) => {
-  //  console.log('add button')
 
     let gain_loss = $('#expense-gain-loss').val().trim()
     let amount = $('#expense-amount').val().trim()
 
     evt.preventDefault();
 
-    if (gain_loss === 'Loss') {
-        amount = -Math.abs(amount);
-      //  console.log(amount)
 
-      
-    } else {
-        console.log('Gain')
-    }
+    //if a user chooses "Loss" the number they enter into the amount input will get converted to negative for the chart
+    if (gain_loss === 'Loss')  amount = -Math.abs(amount);
 
-
+    //this object of data get pushed to an array for the database
     let rowObj = {
         date: $('#expense-date').val().trim(),
         gain_loss: gain_loss,
@@ -28,11 +24,8 @@ $("#expense-form-add-btn").click((evt) => {
         amount: amount,
     }
 
-    console.log(JSON.stringify(rowObj) + "object " + i++)
+    expenseArray.push(rowObj);
 
-    expenseArray.push(rowObj)
-
-   // console.log(expenseArray)
 
     //this will render the html for a new row of form elements on an add button click
     let inputDate = $("<input>", {
@@ -64,18 +57,15 @@ $("#expense-form-add-btn").click((evt) => {
     let deleteBtn = $("<button>", {
         text: "X",
         type: 'button',
-        // onclick: "Delete(this);",
         class: 'delete-btn btn-styles',
         value: del++
+    });
 
-    })
-
+    //this list item is for the empty <ul> element in the html
     let newInputRow = document.createElement('li');
 
-    // let inputDataAtt = jQuery.data(newInputRow, 'inputRow', 'row ' + i++)
-
+    //appending to page
     $(newInputRow).append(inputDate, inputGainLoss, inputDescription, inputAmount, deleteBtn)
-   // console.log(newInputRow)
 
     $('#expense-row-list').append(newInputRow)
 
@@ -83,26 +73,21 @@ $("#expense-form-add-btn").click((evt) => {
     $('#main-expense-row :input').val('')
 });
 
+// ".on('click'," is needed for dynamically created elements 
+// this is specifically listening on the form for the delete button
 $('#expense-row-list').on('click', ".delete-btn", function (evt) {
-
     evt.preventDefault();
 
+    let btnIndex = this.value
+
+    //listens to the delete button the user chose, then deletes the corresponding parent element  //this is for the rendered html
     $(this.parentNode).remove()
 
-    let btnIndex = this.value
-    let test = btnIndex
-
-    // console.log(btnIndex + " btn counter")
-
-    // console.log(expenseArray + " expense array")
-
-    // console.log(test + " test")
-
-
-    expenseArray.splice(test, 1)
+    //this deletes the actual data from the array 
+    expenseArray.splice(btnIndex, 1)
 })
 
- // submit rendered rows button on click listener 2 of 3- chart.js/post request
+ // submit rendered rows button on click listener 2 of 2- chart.js/post request -this sends the expenseArray data to the database & renders the chart
 $('#submit-expense-row').click( async(evt) => {
     evt.preventDefault()
     const res = await fetch('/api/expform', {
@@ -122,39 +107,31 @@ $('#submit-expense-row').click( async(evt) => {
     }
 })
 
-
-
 function chartJS () {
-    dateArray = expenseArray.map((x) => {
-        return x.date
-    })
 
-    intArray = expenseArray.map((x) => {
-        return x.amount
-    })
+    //isolate data from form to use in the graph
+    dateArray = expenseArray.map((x) => { return x.date })
+    intArray = expenseArray.map((x) => { return x.amount })
 
-    const labels = dateArray
-
-
+    //chart data
     const data = {
-        labels: labels,
+        labels: dateArray,
         datasets: [{
             label: 'Daily Expenses',
             backgroundColor: 'rgb(255,122,0)',
             borderColor: 'rgb(255, 99, 132)',
-            // data is money
             data: intArray,
         }]
     };
 
-
+    //just info on the graph being rendered
     const config = {
         type: 'bar',
         data: data,
         options: {}
     };
 
-
+    // creation of the new chart
     const myChart = new Chart(
         document.getElementById('myChart'),
         config
